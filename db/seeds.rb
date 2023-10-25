3c6e73c538a2718c03335033e914ca8eb9550e6b
Nicolas Bolañez <Zven94>
2023-10-25 18:53:53 -0300

    create API with rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+messages = [
+  "Welcome to our app!",
+  "It is a pleasure having you in here.",
+  "Thank you for joining us.",
+  "Welcome to the community.",
+  "We hope you feel at home."
+]
+
+messages.each do |content|
+  Message.create(content: content)
+end
